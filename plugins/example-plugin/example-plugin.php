b5ed7e4fc0818eb3c266db42d226389cea7fa948
Abdul Raffay <abdul-raffay@nytrotech.com>
2020-06-18 17:55:18 +0500

    Example Plugin Added

--- plugins/example-plugin/example-plugin.php
+++ plugins/example-plugin/example-plugin.php
@@ -7,4 +7,15 @@
  * Author: Abdul Raffay Rizwan | Nytrotech
  */
 
+ function example_function(){
+     $content = "Basic Plugin";
+
+     $content .= "<div>Div Tag</div>";
+     $content .= "<p>P Tag</p>";
+
+     return $content;
+ }
+
+ add_shortcode('example', 'example_function');
+
 ?>
